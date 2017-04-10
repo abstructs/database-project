b8eb81413da0fcf9a6c6992e8042c93a171297e9
polygoning <polygoning@gmail.com>
2017-04-10 16:47:05 -0400

    wish i could ssh instead

--- tables.sql
+++ tables.sql
@@ -83,7 +83,7 @@ CREATE TABLE Dependent_T (
     DependentSIN        VARCHAR(11),
 
 CONSTRAINT Dependent_Pk PRIMARY KEY(DependentID),
-CONSTRAINT Dependent_Fk FOREIGN KEY(SalariedID) REFERENCES Salaried_Fk);
+CONSTRAINT Dependent_Fk FOREIGN KEY(SalariedID) REFERENCES Salaried_T(Salaried_Fk));
 
 CREATE TABLE Department_Location_T(
     DepartmentID        INTEGER,
